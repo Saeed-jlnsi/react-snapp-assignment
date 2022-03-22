15a22d2fe23e8cd0839d558a31c57eeaa01a0071
Saeed-jlnsi <saeed.jalinousi@gmail.com>
2022-03-22 15:51:16 +0430

    work on the base layout, routing and containers of the app

---
+++ src/containers/Passengers/Passengers.js
@@ -0,0 +1,13 @@
+import { Container } from '@mui/material';
+
+function Passengers () {
+    return (
+        <Container maxWidth="lg">
+            <p>lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem </p>
+            <p>lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem </p>
+        </Container>
+       
+    )
+}
+
+export default Passengers;
---
+++ src/containers/Profile/Profile.js
@@ -0,0 +1,16 @@
+import { Grid } from '@mui/material';
+
+function Profile () {
+    return (
+        <Grid container spacing={2}>
+            <Grid item>
+                <p>lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem </p>
+            </Grid>
+            <Grid item>
+            <p>lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem </p>
+            </Grid>
+        </Grid>
+    )
+}
+
+export default Profile;
